1bee6b0976674fc8e7b52ef145af01db3088f029
Damien Eversmann <damien.eversmann@gmail.com>
2025-05-31 18:09:53 -0500

    fixing kickoff.sh script

--- .config/yadm/kickoff.sh
+++ .config/yadm/kickoff.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 # New machine kickoff does the following:
-# - make sure macOS Developer Command Line Tools are installed
+# - make sure XCode Command Line Tools are installed
 # - download a temporary copy of yadm
 # - kickoff the yadm download and bootstrap
 
-# Install macOS Developer Command Line Tools if needed
+# Install XCode Command Line Tools if needed
 if ! xcode-select --print-path &>/dev/null; then
     xcode-select --install &>/dev/null
 fi
+
+# Wait until XCode Command Line Tools installation has finished.
+until $(xcode-select --print-path &> /dev/null); do
+    echo '.';
+    sleep 5;
+done
+echo '\n';
 
 # Acquire a temporary copy of yadm
 mkdir -p ~/tmp
