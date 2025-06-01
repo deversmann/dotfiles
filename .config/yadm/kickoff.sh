7c1c5f604776dec6f1d22946d8e01781f4cad3d1
Damien Eversmann <damien.eversmann@gmail.com>
2025-05-31 19:47:25 -0500

    fixing kickoff.sh script

--- .config/yadm/kickoff.sh
+++ .config/yadm/kickoff.sh
@@ -5,28 +5,35 @@
 # - download a temporary copy of yadm
 # - kickoff the yadm download and bootstrap
 
-# Install XCode Command Line Tools if needed
+echo "Installing XCode Command Line Tools if needed."
 if ! xcode-select --print-path &>/dev/null; then
     xcode-select --install &>/dev/null
+    echo -n "Waiting XCode Command Line Tools installation to finish."
+    until $(xcode-select --print-path &> /dev/null); do
+        echo -n '.';
+        sleep 5;
+    done
+    echo;
+else
+    echo "XCode Command Line Tools already installed."
 fi
 
-# Wait until XCode Command Line Tools installation has finished.
-until $(xcode-select --print-path &> /dev/null); do
-    echo -n '.';
-    sleep 5;
-done
-echo;
-
+echo "Updating PATH for Python and persisting to .zprofile"
+export PATH="$(python3 -m site --user-base)/bin:$PATH"
 echo >> ~/.zprofile
 echo "PATH=\"$(python3 -m site --user-base)/bin:\$PATH\"" >> ~/.zprofile
 echo "export PATH" >> ~/.zprofile
 
-# Acquire a temporary copy of yadm
+echo "Acquiring a temporary copy of yadm."
 mkdir -p ~/tmp
 curl -fLo ~/tmp/yadm https://github.com/yadm-dev/yadm/raw/master/yadm && chmod a+x ~/tmp/yadm
 
-# Start the party
+echo "Cloning dotfiles and kicking off bootstrap."
 pushd ~
-~/tmp/yadm clone --bootstrap https://github.com/deversmann/dotfiles
-~/tmp/yadm decrypt
+tmp/yadm clone --bootstrap https://github.com/deversmann/dotfiles
+echo "Decrypting encrypted dotfiles."
+tmp/yadm decrypt
+
+echo "Cleaning up"
+rm tmp/yadm
 popd
